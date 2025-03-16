abfed0a48d893c9dd97dc2f3dff01c48ab7b52ae
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-03-16 06:11:31 -0700

    found my problem and fixed my function

--- app_travel/views.py
+++ app_travel/views.py
@@ -4,8 +4,5 @@ from django.shortcuts import render
 def index_view(request):
     return render(request, "index.html")
 
-def home_view(request):
-    return render(request, "contact.html")
-
 def about_view(request):
     return render(request, "about.html")
