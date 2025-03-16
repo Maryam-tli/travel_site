05ad17175e0312158b7a73de12a73e1b2df9c30a
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-03-16 07:30:15 -0700

    defined two new paths

--- app_travel/urls.py
+++ app_travel/urls.py
@@ -4,4 +4,6 @@ from app_travel.views import *
 urlpatterns = [
     path('', index_view, name="index"),
     path('/about', about_view, name="about"),
+    path('/blog-home', blog_home_view, name="home"),
+    path('/blog-single', blog_single_view, name="single"),
 ]
