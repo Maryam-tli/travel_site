8be122f5694b17141607a5114087e2925c7cd463
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-03-16 07:31:15 -0700

    defined my new functions

--- app_travel/views.py
+++ app_travel/views.py
@@ -6,3 +6,9 @@ def index_view(request):
 
 def about_view(request):
     return render(request, "about.html")
+
+def blog_home_view(request):
+    return render(request, "blog-home.html")
+
+def blog_single_view(request):
+    return render(request, 'blog-single.html')
