48468249fc1f0f5204c5d2848545a8160d2141df
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-03-16 16:10:10 +0330

    added some path in it becuse wanted to test

--- app_travel/urls.py
+++ app_travel/urls.py
@@ -3,4 +3,6 @@ from app_travel.views import *
 
 urlpatterns = [
     path('', index_view, name="index"),
+    path('home/', home_view, name="home"),
+    path('about/', about_view, name="about"),
 ]
--- app_travel/views.py
+++ app_travel/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index_view(request):
     return render(request, "index.html")
+
+def home_view(request):
+    return render(request, "contact.html")
+
+def about_view(request):
+    return render(request, "about.html")
