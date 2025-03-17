995ef6b7d509be1f99095fcf59fae51adbdae8e7
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-03-17 04:23:07 -0700

    added a new path

--- app_travel/urls.py
+++ app_travel/urls.py
@@ -3,7 +3,8 @@ from app_travel.views import *
 
 urlpatterns = [
     path('', index_view, name="index"),
-    path('/about', about_view, name="about"),
-    path('/blog-home', blog_home_view, name="home"),
-    path('/blog-single', blog_single_view, name="single"),
+    path('about', about_view, name="about"),
+    path('blog-home', blog_home_view, name="home"),
+    path('blog-single', blog_single_view, name="single"),
+    path('elements', elements_view, name="elements")
 ]
