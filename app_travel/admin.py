dfdfc16c582549d15d69b4ae3086a738bf65e657
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-03-18 17:46:25 +0330

    defiend a new Admin class and registered it

--- app_travel/admin.py
+++ app_travel/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from app_travel.models import *
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    deta_hierarchy = 'published_date'
+    empty_value_display = '_empty_'
+    fields = ["title","content", "image", "counted_views", "status", "published_date"]
+    list_display = ["title", "status", "published_date", "created_date", "updated_date"]
+    search_fields = ["title", "content"]
+    list_filter = ["status", "published_date"]
+    
+admin.site.register(Post, PostAdmin)
