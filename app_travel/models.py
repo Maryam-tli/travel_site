a2a0995ece2b4d4323e691914173dbea4d662578
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-03-18 17:44:42 +0330

    made my table and columns

--- app_travel/models.py
+++ app_travel/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=220)
+    content = models.TextField()
+    image = models.ImageField(upload_to="Post_default",default="Post_default/my_default.jpeg" )
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['published_date']
+        
+    def __str__(self):
+        return self.title
