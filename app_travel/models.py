6f33fd42702f831d7d690fddd775feeec7d1bb44
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-03-21 23:40:20 +0330

    deleted status field in my table

--- app_travel/models.py
+++ app_travel/models.py
@@ -6,7 +6,6 @@ class Post(models.Model):
     content = models.TextField()
     image = models.ImageField(upload_to="Post_default",default="Post_default/my_default.jpeg" )
     counted_views = models.IntegerField(default=0)
-    status = models.BooleanField(default=False)
     published_date = models.DateTimeField(null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
