6db0c9b7483f24725a0c5582878167d47da3245c
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-03-21 23:41:19 +0330

    deleted status filter in my PostAdmin

--- app_travel/admin.py
+++ app_travel/admin.py
@@ -5,9 +5,9 @@ from app_travel.models import *
 class PostAdmin(admin.ModelAdmin):
     deta_hierarchy = 'published_date'
     empty_value_display = '_empty_'
-    fields = ["title","content", "image", "counted_views", "status", "published_date"]
-    list_display = ["title", "status", "published_date", "created_date", "updated_date"]
+    fields = ["title","content", "image", "counted_views", "published_date"]
+    list_display = ["title", "published_date", "created_date", "updated_date"]
     search_fields = ["title", "content"]
-    list_filter = ["status", "published_date"]
+    list_filter = ["published_date"]
     
 admin.site.register(Post, PostAdmin)
