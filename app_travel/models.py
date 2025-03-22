2e329b0c244f5a6496fb9cdbec1be1837a03efb2
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-03-23 00:52:27 +0330

    added Tag table in my database

--- app_travel/models.py
+++ app_travel/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length=220)
     content = models.TextField()
     image = models.ImageField(upload_to="Post_default",default="Post_default/my_default.jpeg" )
     counted_views = models.IntegerField(default=0)
+    Tags = models.ManyToManyField('Tag')
     published_date = models.DateTimeField(null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
