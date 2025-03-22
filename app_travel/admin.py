6bdaee18cd4269a6377c618b5f0aa6aa776cb233
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-03-23 00:53:38 +0330

    defined TagAdmin

--- app_travel/admin.py
+++ app_travel/admin.py
@@ -2,12 +2,18 @@ from django.contrib import admin
 from app_travel.models import *
 
 # Register your models here.
+class TagAdmin(admin.ModelAdmin):
+    fields = ["name"]
+    list_display = ["name"]
+    
+admin.site.register(Tag, TagAdmin)
+
 class PostAdmin(admin.ModelAdmin):
     deta_hierarchy = 'published_date'
     empty_value_display = '_empty_'
-    fields = ["title","content", "image", "counted_views", "published_date"]
+    fields = ["title","content", "image", "counted_views", "Tags", "published_date"]
     list_display = ["title", "published_date", "created_date", "updated_date"]
-    search_fields = ["title", "content"]
-    list_filter = ["published_date"]
+    search_fields = ["title", "content", "Tags"]
+    list_filter = ["published_date", "Tags"]
     
 admin.site.register(Post, PostAdmin)
