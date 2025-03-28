23ac4217a7642780f25295da9d4ed11f5b102663
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-03-29 00:44:14 +0330

    changed blog_single path to make it dynamic

--- app_travel/urls.py
+++ app_travel/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', index_view, name="index"),
     path('about', about_view, name="about"),
     path('blog-home', blog_home_view, name="home"),
-    path('blog-single', blog_single_view, name="single"),
+    path('<slug:slug>/', blog_single_view, name="single"),
     path('elements', elements_view, name="elements"),
     path('contact', contact_view, name="contact"),
 ]
