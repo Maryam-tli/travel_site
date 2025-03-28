70fd749bd545bebc4cc9b58c557b7cf28709e8d4
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-03-29 00:45:21 +0330

    changed blog_single_view to get slug

--- app_travel/views.py
+++ app_travel/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from app_travel.models import *
 from django.utils import timezone
 
@@ -17,8 +17,13 @@ def blog_home_view(request):
     return render(request, "blog-home.html", context)
 
 
-def blog_single_view(request):
-    return render(request, 'blog-single.html')
+def blog_single_view(request, slug):
+    class_obj = get_object_or_404(Post, slug=slug)
+    class_obj.counted_views += 1
+    context = {
+        'class_obj': class_obj,
+    }
+    return render(request, 'blog-single.html', context)
 
 def elements_view(request):
     return render(request, "elements.html")
