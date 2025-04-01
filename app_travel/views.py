2f61fcc7ee668e8f8e2c62c51ec67b60c8480c07
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-04-01 04:30:28 +0330

    deleted blog functions and transferred to new app called app_blog_travel

--- app_travel/views.py
+++ app_travel/views.py
@@ -9,46 +9,6 @@ def index_view(request):
 def about_view(request):
     return render(request, "about.html")
 
-def blog_home_view(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    context = {
-        'posts': posts
-    }
-    return render(request, "blog-home.html", context)
-
-
-def blog_single_view(request, slug):
-    class_obj = get_object_or_404(Post, slug=slug)
-    class_obj.counted_views += 1
-    class_obj.save(update_fields=['counted_views'])
-    posts = list(Post.objects.filter(published_date__lt=timezone.now()).order_by('-published_date'))
-    
-    try:
-      index = posts.index(class_obj)
-    except ValueError:
-      index = -1
-
-    if index<0:
-        index = 0
-    elif index >= len(posts):
-        index = len(posts) - 1
-    
-    if index > 0:
-        prev_post = posts[index - 1]
-    else:
-        prev_post = None
-    if index < len(posts) - 1:
-        next_post = posts[index + 1]
-    else:
-        next_post = None
-
-    context = {
-        'class_obj': class_obj,
-        'prev_post': prev_post,
-        'next_post': next_post,
-    }
-    return render(request, 'blog-single.html', context)
-
 def elements_view(request):
     return render(request, "elements.html")
 
