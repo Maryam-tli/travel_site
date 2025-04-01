5a1ff009d52b6cf84b4eeb47da61772543140eb3
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-04-01 04:34:51 +0330

    added new path, include all app_blog_travel.urls

--- dj_folder/urls.py
+++ dj_folder/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('app_travel.urls')),
+    path('blog/', include('app_blog_travel.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
