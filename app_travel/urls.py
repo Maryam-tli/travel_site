dfbf8e441b7791eafa227a6c85e7c6507e0f6d8e
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-04-01 04:28:41 +0330

    deleted blog paths in here

--- app_travel/urls.py
+++ app_travel/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from app_travel.views import *
 
+app_name = 'app_travel'
+
 urlpatterns = [
     path('', index_view, name="index"),
     path('about', about_view, name="about"),
-    path('blog-home', blog_home_view, name="home"),
-    path('<slug:slug>/', blog_single_view, name="single"),
     path('elements', elements_view, name="elements"),
     path('contact', contact_view, name="contact"),
 ]
