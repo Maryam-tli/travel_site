4d482bc9edd8359ee39d939dff5ae71214e3313d
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-04-08 22:10:10 +0330

    defined TagAuthor and added author field in PostAdmin

--- app_blog_travel/admin.py
+++ app_blog_travel/admin.py
@@ -8,10 +8,17 @@ class TagAdmin(admin.ModelAdmin):
     
 admin.site.register(Tag, TagAdmin)
 
+class TagAuthor(admin.ModelAdmin):
+    fields = ["name", "image", "about"]
+    list_display = ["name","image", "about"]
+    search_fields = ["name"]
+
+admin.site.register(Author, TagAuthor)
+
 class PostAdmin(admin.ModelAdmin):
     deta_hierarchy = 'published_date'
     empty_value_display = '_empty_'
-    fields = ["title", "content", "slug","image", "counted_views", "Tags", "published_date"]
+    fields = ["title", "content", "slug","image", "counted_views", "Tags", "author", "published_date"]
     list_display = ["title", "published_date", "created_date", "updated_date"]
     search_fields = ["title", "content", "Tags"]
     list_filter = ["published_date", "Tags"]
