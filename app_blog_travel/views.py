e34dde8c1aa0f0793bf2ed85b20e1b350e1a61d1
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-04-08 15:32:56 +0330

    defined Paginator aption in my blog_home_view function

--- app_blog_travel/views.py
+++ app_blog_travel/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from app_blog_travel.models import *
 from django.utils import timezone
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def blog_home_view(request, Tags_item=None):
@@ -17,9 +18,16 @@ def blog_home_view(request, Tags_item=None):
             tag_dict[tag.name] = 1
         else:
             tag_dict[tag.name] += 1
-            
+    paginator = Paginator(posts, 3)
+    try:
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(paginator.num_pages)
 
-    return render(request, 'blog-home.html', {'tag_dict': tag_dict, 'posts': posts})
+    return render(request, 'blog-home.html', {'tag_dict': tag_dict, 'page_obj': page_obj})
 
 
 def blog_single_view(request, slug):
