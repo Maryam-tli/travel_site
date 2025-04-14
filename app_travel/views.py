0315c9dd549b2bba1bc7efbf31109ccd048efdb2
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-04-14 16:29:19 +0330

    added a connection my contact view to forms.py class

--- app_travel/views.py
+++ app_travel/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from app_travel.models import *
 from django.utils import timezone
+from app_travel.forms import *
+from django.contrib import messages
 
 # Create your views here.
 def index_view(request):
@@ -13,4 +15,14 @@ def elements_view(request):
     return render(request, "elements.html")
 
 def contact_view(request):
-    return render(request, "contact.html")
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            contact = form.save(commit=False)
+            contact.name = "Unknown"
+            contact.save()
+            messages.success(request, "Your message has been sent successfully.")
+        else:
+            messages.error(request, "There was an error sending your message. Please try again.")
+    form = ContactForm()
+    return render(request, "contact.html", {'form': form})
