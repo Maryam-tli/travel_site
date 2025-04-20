338c0e0dfb91588744868f62ab2f6aad2eabcd1c
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-04-20 00:59:32 -0700

    changed tag path with category path

--- app_blog_travel/urls.py
+++ app_blog_travel/urls.py
@@ -5,7 +5,7 @@ app_name = 'app_blog_travel'
 
 urlpatterns = [
     path('blog-home', blog_home_view, name="home"),
-    path('Tags/<str:Tags_item>/', blog_home_view, name="Tags"),
+    path('category/<str:category_item>/', blog_home_view, name="category"),
     path('search/',blog_search_view, name="search"),
     path('<slug:slug>/', blog_single_view, name="single"),
 ]
