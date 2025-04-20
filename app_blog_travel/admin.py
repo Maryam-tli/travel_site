efb01755302255b5fff64826d76797b9aa0f1538
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-04-20 00:54:42 -0700

    changed the name of Tags field to Categories field

--- app_blog_travel/admin.py
+++ app_blog_travel/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from app_blog_travel.models import *
 
 # Register your models here.
-class TagAdmin(admin.ModelAdmin):
+class CategoryAdmin(admin.ModelAdmin):
     fields = ["name"]
     list_display = ["name"]
     
-admin.site.register(Tag, TagAdmin)
+admin.site.register(category, CategoryAdmin)
 
 class TagAuthor(admin.ModelAdmin):
     fields = ["name", "image", "about"]
@@ -18,9 +18,9 @@ admin.site.register(Author, TagAuthor)
 class PostAdmin(admin.ModelAdmin):
     deta_hierarchy = 'published_date'
     empty_value_display = '_empty_'
-    fields = ["title", "content", "slug","image", "counted_views", "Tags", "author", "published_date"]
+    fields = ["title", "content", "slug","image", "counted_views", "category", "author", "published_date"]
     list_display = ["title", "published_date", "created_date", "updated_date"]
-    search_fields = ["title", "content", "Tags"]
-    list_filter = ["published_date", "Tags"]
+    search_fields = ["title", "content", "Categories"]
+    list_filter = ["published_date", "Categories"]
     
 admin.site.register(Post, PostAdmin)
