19732d6743ad04f654e4a0c72a5970904951f56c
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-04-29 12:15:30 -0700

    added auth model path

--- dj_folder/urls.py
+++ dj_folder/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('app_travel.urls')),
     path('blog/', include('app_blog_travel.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
