3db757901630fbf7470ac715f9aaabfc6de0b6e3
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-05-02 12:48:53 -0700

    implemented user signup view with email-or-username authentication backend support

--- app_travel/views.py
+++ app_travel/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from app_travel.models import *
 from django.utils import timezone
 from app_travel.forms import *
 from django.contrib import messages
+from django.contrib.auth import login
+from django.contrib.auth import get_backends
+from app_travel.backends import EmailOrUsernameModelBackend
 
 # Create your views here.
 def index_view(request):
@@ -26,3 +29,18 @@ def contact_view(request):
             messages.error(request, "There was an error sending your message. Please try again.")
     form = ContactForm()
     return render(request, "contact.html", {'form': form})
+
+def signup_view(request):
+    form = CustomSignupForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            user = form.save()
+            backend_found = False
+            for backend in get_backends():
+                if isinstance(backend, EmailOrUsernameModelBackend):
+                    user.backend = f"{backend.__module__}.{backend.__class__.__name__}"
+                    backend_found = True
+                    break
+            login(request, user)  # ورود خودکار بعد از ثبت‌نام
+            return redirect('/')  # یا هر صفحه‌ای که خواستی
+    return render(request, 'signup.html', {'form': form})
