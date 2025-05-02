d914b71ec31eb3fdc754a0add0a772236f752db7
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-05-02 12:46:51 -0700

    defined CustomSignupForm to also collect the user's email

--- app_travel/forms.py
+++ app_travel/forms.py
@@ -1,7 +1,29 @@
+from django import forms
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from app_travel.models import *
 
 class ContactForm(ModelForm):
     class Meta:
         model = Contact
         fields = '__all__'
+    
+class CustomSignupForm(UserCreationForm):
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
+        }
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
