2a107bc03c70b4de935ac5e05c05d6344e329aa6
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-05-02 09:00:55 -0700

    added AUTHENTICATION_BACKENDS in settings

--- dj_folder/settings.py
+++ dj_folder/settings.py
@@ -142,3 +142,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'app_travel.backends.EmailOrUsernameModelBackend',
+]
