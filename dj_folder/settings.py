4343517f491baae35151c501f37160eca204c8d4
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-05-02 15:18:43 -0700

    moved email credentials to .env settings.py

--- dj_folder/settings.py
+++ dj_folder/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -30,15 +31,13 @@ ALLOWED_HOSTS = []
 # auth model
 LOGIN_REDIRECT_URL = '/'
 
-#backend anymail
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.zoho.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'maryamtli@zohomail.com'  # ← ایمیلی که ساختی
-EMAIL_HOST_PASSWORD = "Mary949494" # ← پسورد ایمیل
-DEFAULT_FROM_EMAIL = 'Maryam Travel <maryamtli@zohomail.com>'
-
+# email
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 # Application definition
 
